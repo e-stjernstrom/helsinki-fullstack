6c07c69b04e647ca4d645446ce7d97d32230eacf
Erika Stjernstrøm <e.stjernstrom@outlook.com>
2025-07-18 16:19:01 +1000

    Finished Exercises up to 1.10.

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -5,18 +5,22 @@ const Button = (props) => (
   </button>
 )
 
+const StatisticLine = ({ text, value }) => {
+  return <p>{text} {value}</p>
+}
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   const avg = (1 * good + 0 * neutral + (-1) * bad) / total
   const content = total !== 0
   ? (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {avg}</p>
-      <p>positive {(good / total) * 100} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={avg} />
+      <StatisticLine text="positive" value={(good / total) * 100 + '%'} />
     </div>
   ) : (<p>No feedback given</p>)
   return (
