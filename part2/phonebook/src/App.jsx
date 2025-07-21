e4c0b760b90fd0a02b785cfeaa8129302eb8b163
Erika Stjernstrøm <e.stjernstrom@outlook.com>
2025-07-21 17:39:09 +1000

    Finished Exercices 2.8.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,24 +2,32 @@ import { useState } from 'react'
 const Persons = ({ persons }) => {
   return (
     <ul>
-      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      {persons.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
     </ul>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const addNewPerson = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber, id: persons.length + 1 }
     const isDuplicateName = persons.some(person => person.name === newName)
     
     if (isDuplicateName) {
@@ -29,15 +37,15 @@ const App = () => {
     }
 
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
