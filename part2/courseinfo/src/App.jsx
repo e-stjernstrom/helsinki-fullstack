ed13b8ff192289c5143d87e2f2e85157198eea80
Erika Stjernstrøm <e.stjernstrom@outlook.com>
2025-07-21 14:43:47 +1000

    Finished Exercices 2.1.

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,58 @@
+const Header = ({ course }) => {
+  return(
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <li>{part.name} {part.exercises}</li>
+  )
+}
+
+const Content = ({ course }) => {
+  const content = course.parts.map(part => <Part key={part.id} part={part}/>)
+  return(
+    <ul>{content}</ul>
+  )
+}
+
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
