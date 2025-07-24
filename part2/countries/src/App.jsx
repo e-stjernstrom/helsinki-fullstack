5c7150053ba02c1e1b022c65f5dab724c5486cfe
Erika Stjernstrøm <e.stjernstrom@outlook.com>
2025-07-24 19:39:13 +1000

    Finished Exercices 2.18.

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const ShowCountry = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>Capital {country.capital.map(capital => (
+        <span key={capital}>{capital} </span>
+      ))}</p>
+      <p>Area {country.area}</p>
+      <h3>Languages</h3>
+      <ul>{Object.values(country.languages).map(language => (
+        <li key={language}>{language}</li>
+      ))}</ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
+    </div>
+  )
+}
+
+const DisplayCountries = ({ countries, filter }) => {
+  const filtered = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+
+  if (filtered.length > 10) {
+    return (<div>Too many matches, specify another filter</div>)
+  }
+
+  if (filtered.length === 1) {
+    return (<ShowCountry country={filtered[0]} />)
+  }
+
+  return (
+    <div>
+      {filtered.map(country => (
+        <div key={country.name.official}>{country.name.common}</div>
+      ))}
+    </div>
+  )
+}
+
+function App() {
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+  
+
+  return (
+    <div>
+      <p>find countiees <input value={filter} onChange={handleFilterChange} /></p>
+      <DisplayCountries countries={countries} filter={filter}/>
+    </div>
+  )
+}
+
+export default App
