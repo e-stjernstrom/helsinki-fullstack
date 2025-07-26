5dae2b74bb183dd7a8e0b5f5b874cff2483581a3
e-stjernstrom <e.stjernstrom@outlook.com>
2025-07-26 23:06:17 +1000

    Fixed countries city name logic in weather

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -26,7 +26,7 @@ const CityWeather = ({ city }) => {
 
   return (
     <div>
-      <h3>Weather in {weather.name}</h3>
+      <h3>Weather in {city}</h3>
       <p>Temperature {kelvinToCelsius(weather.main.temp)} Celsius</p>
       <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
       <p>Wind {weather.wind.speed} m/s</p>
