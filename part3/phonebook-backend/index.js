009c01cffb0ce078bb154d6b7ea9696c018679f1
e-stjernstrom <e.stjernstrom@outlook.com>
2025-07-28 02:10:07 +1000

    Finished Exercises 3.6.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -24,6 +26,13 @@ let persons = [
     }
 ]
 
+app.get('/info', (req, res) => {
+  res.send(`
+    <div>Phonebook has info for ${persons.length} people</div>
+    <div>${new Date()}</div>
+  `)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
@@ -45,11 +54,38 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
-app.get('/info', (req, res) => {
-  res.send(`
-    <div>Phonebook has info for ${persons.length} people</div>
-    <div>${new Date()}</div>
-  `)
+
+
+const genID = (scale = 2) => {
+  const random = Math.floor(Math.random() * Math.pow(10, scale)).toString().padStart(scale, '0')
+  const id = Date.now().toString() + random
+  return id
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json(
+      { error: 'name or number missing' }
+    )
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return res.status(400).json(
+      { error: 'name must be unique' }
+    )
+  }
+
+  const person = {
+    id: genID(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person) // response output
 })
 
 const PORT = 3001
